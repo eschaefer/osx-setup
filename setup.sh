e3eed21af5126efff5b24b5299c4d28692f19ab1
Eric Schaefer <omg@eric-schaefer.com>
2018-06-01 15:15:22 +0200

    Update nvm and rvm scripts

--- setup.sh
+++ setup.sh
@@ -95,11 +95,11 @@ brew cask install ${apps[@]}
 brew cleanup
 
 # RVM
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 
 # NVM
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # If it runs...
 nvm install 8 --default
